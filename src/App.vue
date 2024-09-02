86a3c0665e17af66341cae56b33c01b008a7d5ec
Charles McCullough <charles@charlesmccullough.com>
2024-09-01 21:25:41 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import NavbarHead from './components/NavBar.vue';
+  import HeroBody from './components/HeroBody.vue';
+  import HomeCards from './components/CardHome.vue';
+  import JobListings from './components/JobListings.vue';
+</script>
+
+<template>
+  <NavbarHead />
+  <HeroBody />
+  <HomeCards />
+  <JobListings :limit="3" :show-button="true" />
+</template>
+
+<style scoped>
+
+</style>
